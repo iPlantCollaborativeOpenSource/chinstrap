3cf0e09247a188da51ac21dbfb5da60626ad5411
Roey Chasman <rchasman@email.arizona.edu>
2012-06-20 12:29:55 -0700

    Began initial testing of noir and hiccup.

--- src/chinstrap/views/common.clj
+++ src/chinstrap/views/common.clj
@@ -1,12 +1,14 @@
 (ns chinstrap.views.common
-  (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+    (:use [noir.core :only [defpartial]]
+          [hiccup.page :only [include-css html5]]))
 
 (defpartial layout [& content]
-            (html5
-              [:head
-               [:title "chinstrap"]
-               (include-css "/css/reset.css")]
-              [:body
-               [:div#wrapper
+    (html5
+        [:head
+            [:title "chinstrap"]
+            (include-css "/css/reset.css"
+                         "/css/style.css"
+                         "http://fonts.googleapis.com/css?family=Gloria+Hallelujah")]
+        [:body
+            [:div#wrapper
                 content]]))
--- src/chinstrap/views/welcome.clj
+++ src/chinstrap/views/welcome.clj
@@ -1,8 +1,11 @@
 (ns chinstrap.views.welcome
-  (:require [chinstrap.views.common :as common]
-            [noir.content.getting-started])
+  (:require [chinstrap.views.common :as common])
   (:use [noir.core :only [defpage]]))
 
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to chinstrap"]))
+(defpage "/" []
+    (common/layout
+        [:h1 "The Chinstrap has you."]
+        [:h2 "The Chinstrap has you."]
+        [:h3 "The Chinstrap has you."]
+        [:h4 "The Chinstrap has you."]
+    ))
