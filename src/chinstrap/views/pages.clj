709d7fbebf7766e9e28da660ba5b3ded0ef1694b
Roey Chasman <rchasman@email.arizona.edu>
2012-06-20 13:23:58 -0700

    Added the logo to the status page.

--- src/chinstrap/views/pages.clj
+++ src/chinstrap/views/pages.clj
@@ -1,10 +1,18 @@
 (ns chinstrap.views.pages
   (:require [chinstrap.views.common :as template])
-  (:use [noir.core :only [defpage]]))
+  (:use [noir.core :only [defpage]]
+        [chinstrap.db]
+        [hiccup.element]))
 
 (defpage "/" []
     (template/start
         [:br][:br][:br][:br][:br][:br]
         [:br][:br][:br][:br][:br][:br]
+        [:h1 "The Chinstrap has you."]
+    ))
+
+(defpage "/status" []
+    (template/page
+        (image "/img/logo.png")
         [:h1 "The Chinstrap has you."]
     ))
