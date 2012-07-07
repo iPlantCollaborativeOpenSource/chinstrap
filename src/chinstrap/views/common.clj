0935d20fe6223b9ac40fa9031d49f29020959958
Roey Chasman <rchasman@email.arizona.edu>
2012-07-06 18:00:18 -0700

    Added a new page, 'main', which will house the general DE status info based on the date you pick.

--- src/chinstrap/views/common.clj
+++ src/chinstrap/views/common.clj
@@ -3,6 +3,22 @@
         [hiccup.page]
         [hiccup.element]))
 
+(defpartial main-page [& content]
+    (html5
+      [:head
+        [:title "Discovery Enviroment Status"]
+        (include-css "/css/reset.css"
+                     "/css/style.css")
+        (include-js  "/js/get-info.js"
+                     "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
+                     "http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.js"
+                     "/js/collapsible-panel.js")]
+      [:body
+        (javascript-tag "$(document).ready(function(){
+          $('.collapsibleContainer').collapsiblePanel();
+          $('#date').datepicker();})")
+[:div#wrapper content]]))
+
 (defpartial apps-page [& content]
     (html5
       [:head
--- src/chinstrap/views/pages.clj
+++ src/chinstrap/views/pages.clj
@@ -16,7 +16,23 @@
       (mc/find-maps "jobs" {:state.status (str state)} [:state.name])))
 
 (defpage "/" []
-  (render "/apps"))
+  (render "/main"))
+
+(defpage "/main" []
+  (template/main-page
+    (image "/img/logo.png")
+    [:h3 "Discovery Environment Status"]
+    [:br]
+    [:h4 "Pick a date"
+      [:span
+        [:input#date {:type "text" :onInput "getInfo()"}]]]
+    [:br]
+    [:div#inner]
+    [:br]
+    (link-to "/components" "Discovery Environment Component Info")
+    [:br]
+    (link-to "/apps" "Discovery Environment App Info")
+    [:br]))
 
 ;Page listing the count of different states of Discovery Environment Apps.
 (defpage "/apps" []
@@ -26,9 +42,9 @@
     [:h3 "Discovery Environment App Status"]
     [:br]
     [:div#inner
-      [:h3.left "Running Apps:" [:span#running]]
-      [:h3.left "Submitted Apps:" [:span#submitted]]
-      [:h3.left "Completed Apps:" [:span#completed]]]
+      [:h3.left "Running Apps:" [:span#running.right]]
+      [:h3.left "Submitted Apps:" [:span#submitted.right]]
+      [:h3.left "Completed Apps:" [:span#completed.right]]]
     [:br]
     [:div.collapsibleContainer {:title "Currently Running Apps"}
       [:div (apps-that-are "Running")]]
@@ -36,9 +52,10 @@
     [:div.collapsibleContainer {:title "Submitted Apps"}
       [:div (apps-that-are "Submitted")]]
     [:br]
-    (link-to "/components" "Discovery Environment Component Info")
+    (link-to "/main" "Discovery Environment Status")
     [:br]
-    ))
+    (link-to "/components" "Discovery Environment Component Info")
+    [:br]))
 
 ;Page listing count and info of Components with no transformation activities.
 (defpage "/components" []
@@ -50,9 +67,9 @@
     [:h3 "Discovery Environment Components Info"]
     [:br]
     [:div#inner
-      [:h3.left "With Associated Apps:" [:span#with]]
-      [:h3.left "Without Associated Apps:" [:span#without]]
-      [:h3.left "Total Components:" [:span#all]]]
+      [:h3.left "With Associated Apps:" [:span#with.right]]
+      [:h3.left "Without Associated Apps:" [:span#without.right]]
+      [:h3.left "Total Components:" [:span#all.right]]]
       [:br]
       [:div.collapsibleContainer {:title "Unused Componenent Details"}
       [:br]
@@ -77,6 +94,8 @@
                  [:td.center (var-get (def j (inc j)))]
                  [:td (:name list)]
                  [:td.center (:count list)]])]]
+      [:br]
+      (link-to "/main" "Discovery Environment Status")
       [:br]
       (link-to "/apps" "Discovery Environment App Status")
       [:br]))
