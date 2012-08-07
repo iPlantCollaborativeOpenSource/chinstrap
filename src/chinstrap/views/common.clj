c92146a86bb64291f53a8de5acb1c3719a3782de
Roey Chasman <rchasman@email.arizona.edu>
2012-08-07 15:14:28 -0700

    CSV file rename

--- src/chinstrap/views/common.clj
+++ src/chinstrap/views/common.clj
@@ -12,7 +12,7 @@
     (include-js
       "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
       "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"
-      "/js/CSV-parser.js")])
+      "/js/csv-parser.js")])
 
 (defpartial navbar []
   [:div#navbar
