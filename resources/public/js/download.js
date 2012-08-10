779cdacb97587926e8a525f6dbf81801f2e0a729
Roey Chasman <rchasman@email.arizona.edu>
2012-08-10 15:40:53 -0700

    Removal of uneccesary files. Minor style changes.

--- resources/public/js/download.js
+++
@@ -1,9 +0,0 @@
-function download(filename, data) {
-    var blob = new Blob([data]);
-    var evt = document.createEvent("HTMLEvents");
-        evt.initEvent("click");
-        $("<a>", {
-            download: filename,
-            href: webkitURL.createObjectURL(blob)
-            }).get(0).dispatchEvent(evt);
-};
