ddcf34112a10082b0fadb291f2caf89291a0a516
Roey Chasman <rchasman@email.arizona.edu>
2012-09-24 19:37:12 -0700

    Added keybinding support to traverse the integrators and changed the design of the integrator picker.

--- resources/public/js/get-integrators.js
+++ resources/public/js/get-integrators.js
@@ -1,12 +1,17 @@
-function getIntegrators(who) {
-    $.get('get-integrator-data/' + who, function(resp){
-        body =
-            "<h3><strong>" + resp['integrator_name'] + "</h3></strong>"
-            + "<hr>"
-            + "<h4 class='left'>Email:"
-            + "<span class='right'>" + resp['integrator_email'] + "</span></h4>"
-            + "<h4 class='left'>Integrator ID:"
-            + "<span class='right'>" + resp['id'] + "</span></h4>"
-        $('#inner').html(body)
-    })
+function getIntegrator(who) {
+    if (!who || who == "General Data"){
+        generalData();
+    }else{
+        $.get('get-integrator-data/' + who, function(resp){
+            body =
+                "<h3><strong>" + resp['integrator_name'] + "</h3></strong>"
+                + "<hr>"
+                + "<h4 class='left'>Email:"
+                + "<span class='right'>"
+                + resp['integrator_email'] + "</span></h4>"
+                + "<h4 class='left'>Integrator ID:"
+                + "<span class='right'>" + resp['id'] + "</span></h4>"
+            $('#inner').html(body)
+        })
+    }
 }
