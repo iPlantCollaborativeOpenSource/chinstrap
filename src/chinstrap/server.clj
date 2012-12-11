bd2a03ce62792698d70fdad6912c5b51f2daee85
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-12-11 08:05:36 -0700

    CORE-3790: Added try-catch around configuration call.
    This should prevent chinstrap from crashing hard if mongo is
    unreachable.

--- src/chinstrap/server.clj
+++ src/chinstrap/server.clj
@@ -2,12 +2,17 @@
   (:gen-class)
   (:use [chinstrap.config :only [listen-port]]
         [chinstrap.db])
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [clojure.tools.logging :as log]))
 
 (server/load-views-ns 'chinstrap.views)
 
 (defn -main [& m]
-  (db-config)
+  (try
+    (db-config)
+    (catch Exception e
+      (log/error e "Configuration Failed")))
+  
   (let [mode (keyword (or (first m) :dev))
         port (listen-port)]
     (server/start port {:mode mode
