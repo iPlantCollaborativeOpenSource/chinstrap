f391792755befb65d817bddbf2c49f914fff98dd
Dennis Roberts <dennis@iplantcollaborative.org>
2013-01-29 15:42:52 -0700

    CORE-3924: migrated to Korma version 0.3.0-RC2

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Diagnostic and status front-end for backend data"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.iplantc/clojure-commons "1.1.0-SNAPSHOT"]
-                 [org.iplantc/kameleon "0.0.1-SNAPSHOT"]
+                 [org.iplantc/kameleon "0.1.0-SNAPSHOT"]
                  [clj-time "0.4.4"]
                  [org.clojure/data.json "0.1.2"]
                  [com.novemberain/monger "1.1.2"]
-                 [korma/korma "0.3.0-beta10"]
+                 [korma/korma "0.3.0-RC2"]
                  [log4j/log4j "1.2.16"]
                  [noir "1.3.0-beta10"]]
   :plugins [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
