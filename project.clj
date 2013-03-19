1dbe1bae37b3666859accff5b78f1b4ba9a52655
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-19 16:32:26 -0700

    upped dependencies and increased version
    lein-iplant-rpm -> 1.4.1-SNAPSHOT
    clojure -> 1.5.0
    clojure-commons -> 1.4.1-SNAPSHOT
    kameleon -> 0.1.1-SNAPSHOT
    clj-time -> 0.4.5
    monger -> 1.4.2
    log4j -> 1.2.17

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject chinstrap "0.2.0-SNAPSHOT"
+(defproject chinstrap "0.2.1-SNAPSHOT"
   :description "Diagnostic and status front-end for backend data"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.iplantc/clojure-commons "1.4.0-SNAPSHOT"]
-                 [org.iplantc/kameleon "0.1.0-SNAPSHOT"]
-                 [clj-time "0.4.4"]
-                 [com.novemberain/monger "1.1.2"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
+                 [org.iplantc/kameleon "0.1.1-SNAPSHOT"]
+                 [clj-time "0.4.5"]
+                 [com.novemberain/monger "1.4.2"]
                  [korma/korma "0.3.0-RC2"]
-                 [log4j/log4j "1.2.16"]
+                 [log4j/log4j "1.2.17"]
                  [noir "1.3.0-beta10"]]
-  :plugins [[org.iplantc/lein-iplant-rpm "1.4.0-SNAPSHOT"]]
+  :plugins [[org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"]]
   :profiles {:dev {:resource-paths ["resources/conf/test"]}}
   :aot [chinstrap.server]
   :main chinstrap.server
